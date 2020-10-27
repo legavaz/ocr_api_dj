e4e61aff93b245a69b6b34b8f36c51598d54570c
dvi <legavaz@gmail.com>
2020-10-27 17:37:33 +0300

    запуск модуля оср

--- ocr_tools/ocr_tools_main.py
+++ ocr_tools/ocr_tools_main.py
@@ -1,46 +1,61 @@
-﻿import sys,os
+﻿import sys
+import os
 import subprocess
+import tempfile
 from pdf2image import convert_from_path
-from .ot_tools import get_files
-
-# Для Windows
-def return_name(file_name):
-    f_n =   'file_name'
-    arr_f_n =   os.path.basename(file_name).split('.')
-    if len(arr_f_n)>0:
-        f_n =   arr_f_n[0]
-        f_n =   f_n.replace(' ','_')
-
-    return f_n
-
-def png_to_txt(file_name,path):
-    out_put = path+'\\'+return_name(file_name)
-    lang = '-l rus+eng'
-    command = 'tesseract.exe {0} {1} {2}'.format(file_name, out_put, lang)
-    print('COMMAND: ', command)
-    subprocess.call(command)
+from ot_tools import get_files, mk_dir
 
 
-def ocr(filename, path):
-    images = convert_from_path(filename)
-    i = 0
-    f_n = return_name(filename)
-    for target_list in images:
-        i = i+1
-        f_name = path+'\\'+f_n+'_{0}.png'.format(i)
-        target_list.save(f_name)
-        png_to_txt(f_name,path)
+class ocr_obj():
+    """
+    Объект файл 
+    обработка объекта
+    """
+
+    def return_name(self, file_name):
+        f_n = 'file_name'
+        arr_f_n = os.path.basename(file_name).split('.')
+        if len(arr_f_n) > 0:
+            f_n = arr_f_n[0]
+            f_n = f_n.replace(' ', '_')
+        return f_n
+
+    def __init__(self, filename):
+        self.filename = filename
+        self.short = self.return_name(filename)
+
+    def png_to_txt(self, imagefile,path):
+        out_put = path+'\\'+self.return_name(imagefile)
+        lang = '-l rus+eng'
+        command = 'tesseract.exe {0} {1} {2}'.format(
+            imagefile, out_put, lang)
+        print('COMMAND: ', command)
+        subprocess.call(command)
+
+    def ocr(self, path):
+        images = convert_from_path(self.filename)
+        i = 0
+        f_n = self.short
+
+        for target_list in images:
+            i = i+1
+            f_name = path+'\\'+f_n+'_{0}.png'.format(i)
+            target_list.save(f_name)
+            self.png_to_txt(f_name, path)
 
 
-# main
-# file_in 	= r'C:\Users\dvi\Desktop\Договор ФА000134 от 08.05.2020.pdf'
-# out_path 	= '\Temp\out'
-    
-DIR_NAME = r'\\l-pack\net\Сканер\Dergunov'
-m = get_files(DIR_NAME)
-print(m)
 
-# print(return_name(file_in))
+DIR_NAME = r'\\l-pack\net\Сканер\OFF2-Programmisty'
+files = get_files(DIR_NAME)
+temp_dir    =   'temp'
+mk_dir(temp_dir)
+for file in files:
+    with tempfile.TemporaryDirectory() as temp_path:
+        print(temp_path)
+        m = ocr_obj(file)
+        # m.ocr(temp_path)
+        m.ocr(temp_dir)
+        
+
 
 # ocr(file_in, out_path)
-
--- ocr_tools/ot_tools.py
+++ ocr_tools/ot_tools.py
@@ -1,16 +1,34 @@
 import os
+import tempfile
 
-def get_files(m_DIR_NAME,ext = ('pdf,jpg,png')):    
-    files   =   []
+
+def get_files(m_DIR_NAME, ext=('pdf,jpg,png')):
+    files = []
     with os.scandir(m_DIR_NAME) as listFiles:
         for file in listFiles:
-            if file.name[-3] in ext:                
-                    files.append(file.path)
+            if file.name[-3] in ext:
+                files.append(file.path)
     return files
 
 
-if __name__ == "__main__":    
-    DIR_NAME = r'\\l-pack\net\Сканер\Dergunov'
-    m = get_files(DIR_NAME)
-    print(m,len(m),'файла')
+def mk_dir(m_path):
     
+    # with tempfile.TemporaryDirectory() as directory:
+    #     print('Создана временная директория %s' % directory)
+    
+    result = False
+    path = os.getcwd()  
+    try:
+        os.makedirs(path+m_path)
+        result = True
+    except OSError:
+        print("Создать директорию %s не удалось" % m_path)
+    return result
+
+
+if __name__ == "__main__":
+    # DIR_NAME = r'\\l-pack\net\Сканер\Dergunov'
+    # m = get_files(DIR_NAME)
+    # print(m, len(m), 'файла')
+    out_path 	= '\Temp\out'
+    res = mk_dir(out_path)
